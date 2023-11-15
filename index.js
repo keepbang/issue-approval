a35d9dcc42945c63a9976e37898c46cda2c13748
keepbang <gb.chae@mcorpor.com>
2023-11-15 17:27:43 +0900

    feat: separate function

--- index.js
+++ index.js
@@ -6,32 +6,41 @@ async function run() {
   const approvedWords = ["yes", "y", "approve"];
   const deniedWords = ["denied", "deny", "no", "n"];
 
-  try {
-    const secretToken = core.getInput("secret-token");
-    const title = 'Approval required for workflow run : '
-        + github.context.runId;
-    const body = `Respond [${approvedWords.toString()}] to continue workflow or [[${deniedWords.toString()}]] to cancel.`;
-    const approvers = core.getInput("approvers").split(",");
-
-    const octokit = github.getOctokit(secretToken);
-
+  async function createIssue(octokit, title, body, approvers) {
     const issue = await octokit.request('POST /repos/{owner}/{repo}/issues', {
       ...github.context.repo,
       title: title,
       body: body,
       assignees: approvers,
     })
+    return issue.number;
+  }
 
-    core.info(`create issue ${issue.number}`);
+  async function getComments(octokit, issueNumber) {
+    const comments = await octokit.request(
+        'GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+          ...github.context.repo,
+          issue_number: issueNumber
+        });
+    return comments;
+  }
+
+  try {
+    const secretToken = core.getInput("secret-token");
+    const title = 'Approval required for workflow run : '
+        + github.context.runId;
+    const body = `Respond [${approvedWords.toString()}] to continue workflow or [${deniedWords.toString()}] to cancel.`;
+    const approvers = core.getInput("approvers").split(",");
+
+    const octokit = github.getOctokit(secretToken);
+    const issueNumber = await createIssue(octokit, title, body, approvers);
+
+    core.info(`create issue ${issueNumber}`);
 
     let approve = true;
 
     do {
-      const comments = await octokit.request(
-          'GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-            ...github.context.repo,
-            issue_number: issue.number
-          });
+      const comments = await getComments(octokit, issueNumber);
 
       if (comments.length) {
 
@@ -42,7 +51,7 @@ async function run() {
         if (approvedWords.includes(word)) {
           // 승인
           approve = false;
-          core.info(`approve issue ${issue.number}`);
+          core.info(`approve issue ${issueNumber}`);
         } else if (deniedWords.includes(word)) {
           // 거부
           approve = false;
